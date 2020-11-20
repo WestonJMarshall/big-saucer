a58e2cf3ee1f69c8b5e1e347b478a774ec43cb55
Dan Robinson <djr5851@rit.edu>
2020-11-20 18:38:52 -0500

    music added to start screen

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    public GameObject musicPrefab;
+    private GameObject sceneMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        sceneMusic = GameObject.FindGameObjectWithTag("Music");
+        if (sceneMusic == null)
+        {
+            sceneMusic = Instantiate(musicPrefab);
+            DontDestroyOnLoad(sceneMusic);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
